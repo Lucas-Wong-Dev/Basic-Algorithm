5346c05887ca797cb2df2326229ffe777fb421ba
vpakati <34790891+vpakati@users.noreply.github.com>
2020-01-21 22:02:31 +0800

    jan21

--- chapter8/AssignArray.cpp
+++ chapter8/AssignArray.cpp
@@ -1,3 +1,6 @@
+// 数组赋值
+// 注意根据题意，循环中是可以使用一个用于循环计数的局部变量的
+// 
 #include <iostream>
 using namespace std;
 int main()
@@ -14,7 +17,7 @@ int main()
     for (int i = sizeof(a) / sizeof(a[0]) - 2; i > 0;i --)
     {
         b[0] *= a[i + 1];
-        b[i] *= b[0];
+        b[i] *= b[0]; // 注意这里是*=，而不是=
     }
     b[0] *= a[1];
     for (int i = 0; i < sizeof(b) / sizeof(b[0]); i++)
